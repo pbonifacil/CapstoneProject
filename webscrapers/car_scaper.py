101390b38dc67c89e50e0c0104b2c8cac64cc617
pedro.bonifacio <20211652@novaims.unl.pt>
2023-11-09 08:40:16 +0000

    uploaded car scraper

---
+++ webscrapers/car_scaper.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from StringProgressBar import progressBar
+import pickle
+import pandas as pd
+import time
+
+
+def scrap_car_specs(links_to_listings):
+    geckodriver_path = r'.\selenium\geckodriver.exe'
+    dr = webdriver.Firefox(
+        service=webdriver.firefox.service.Service(executable_path=geckodriver_path))  # initialize firefox web driver
+    dataset = []
+    for link in links_to_listings:
+        dr.get(link)
+        try:
+            price_comparison = WebDriverWait(dr, 4).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, "p.esicnpr2"))).text  # Price comparison is dynamic content and takes some time to load so we need to wait before extracting the element
+        except Exception as e:
+            price_comparison = "Não disponível"
+
+        soup = BeautifulSoup(dr.page_source, "html.parser")
+
+        specs_html = soup.find_all("div", class_="ooa-13nyoul e1iqsx44")
+        specs = {list(pair.children)[0].text: list(pair.children)[1].text for pair in specs_html}
+
+        try:
+            specs['Foto'] = soup.find("img", class_="ooa-jcur4u e1ejyjdh16").get("src")
+        except Exception as e:
+            specs['Foto'] = "Não disponível"
+        if not soup.find("h3", class_="offer-price__number esicnpr5 ooa-17vk29r er34gjf0"):
+            print("Listing doesn't exist")
+            continue
+        specs['Preço'] = soup.find("h3", class_="offer-price__number esicnpr5 ooa-17vk29r er34gjf0").text
+        specs['Link'] = link
+        specs['PreçoComparado'] = price_comparison
+        try:
+            specs['Morada'] = soup.find_all("a", class_="eavtgmy0 ooa-1j0jeo9")[2].text
+        except Exception as e:
+            specs['Morada'] = 'Não disponível'
+        specs['Acessórios'] = [accessory.text for accessory in soup.find_all("p", class_="evccnj10 ooa-1i4y99d er34gjf0")]
+
+        dataset.append(specs)
+        # PROGRESS BAR
+        progress_bar = progressBar.filledBar(len(links_to_listings), len(dataset))
+        print(progress_bar[0] + " - " + str(round(progress_bar[1], 2)) + f"%  -  {len(dataset)} cars scraped...",
+              end="\r")
+        time.sleep(2)
+    dr.close()
+    return dataset
+
+
+if __name__ == "__main__":
+    with open("listing_links.pkl", "rb") as fa:
+        links = pickle.load(fa)
+    data = scrap_car_specs(links)
+    df = pd.DataFrame(data)
+    df.to_csv("car_dataset.csv", encoding='utf-8-sig')
