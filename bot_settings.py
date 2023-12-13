15e1f9155d82c281e2bbc933bc2d4510e54bff5f
pedro.bonifacio <20211652@novaims.unl.pt>
2023-12-13 15:23:19 +0000

    deleted unused files

--- bot_settings.py
+++
@@ -1,5 +0,0 @@
-bot_settings = [
-    {"name": "Car Finder Bot",
-     "prompt": """TASK: You are Car Finder Bot, an automated service to help people find a car. Before you start, please read the following instructions carefully.
-"""}
-]
