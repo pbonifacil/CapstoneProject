1980228c46501c5081de73ce1f460ffa36c16117
pedro.bonifacio <20211652@novaims.unl.pt>
2024-01-02 17:08:39 +0000

    added API KEY requirement to login form

--- webpage/chatbot.py
+++ webpage/chatbot.py
@@ -1,12 +1,14 @@
 import time
 import streamlit as st
 from chatbot_util.agent import CarDealerChatbot
-from dotenv import load_dotenv, find_dotenv
+# from dotenv import load_dotenv, find_dotenv
 import hmac
 import csv
 from langchain.schema import AIMessage
+import os
 
-load_dotenv(find_dotenv())
+
+# load_dotenv(find_dotenv())
 
 
 def initialize() -> None:
@@ -36,8 +38,6 @@ def initialize() -> None:
     if "chatbot" not in st.session_state:
         st.session_state.chatbot = CarDealerChatbot(path="chatbot_util/car_dataset.csv")
 
-
-
     with st.sidebar:
         st.markdown(
             f"ChatBot in use: <font color='cyan'>{st.session_state.chatbot.__str__()}</font>", unsafe_allow_html=True
@@ -48,6 +48,7 @@ def initialize() -> None:
 
 def check_password():
     """Returns True if the password is correct, otherwise returns False."""
+
     def load_user_data(csv_path):
         """Load user data from a CSV file."""
         user_data = {}
@@ -60,13 +61,21 @@ def check_password():
     def login_form():
         """Form with widgets to collect user information"""
         with st.form("Credentials"):
-            st.text_input("Username", key="username")
-            st.text_input("Password", type="password", key="password")
-            st.form_submit_button("Log in", on_click=password_entered)
+            username = st.text_input("Username", key="username")
+            password = st.text_input("Password", type="password", key="password")
+
+            api_key = st.text_input("Enter your GPT API key", type="password")
+            os.environ["OPENAI_API_KEY"] = api_key.lstrip('"').rstrip('"')
+
+            if st.form_submit_button("Log in") and username and password and api_key:
+                password_entered()
+            else:
+                st.warning("Please enter all credentials.")
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        user_data = load_user_data("./chatbot_util/customer_data.csv")  # because we use st.stop, even if this was run outside of check_password, this would always be re-executed if password was incorrect, so I left it here for easier reading
+        user_data = load_user_data(
+            "./chatbot_util/customer_data.csv")  # because we use st.stop, even if this was run outside of check_password, this would always be re-executed if password was incorrect, so I left it here for easier reading
         if st.session_state["username"] in user_data and hmac.compare_digest(
                 st.session_state["password"],
                 user_data[st.session_state["username"]]['Password'],
@@ -76,11 +85,11 @@ def check_password():
             st.session_state["show_login_form"] = False
             del st.session_state["password"]  # Don't store the username or password.
             del st.session_state["username"]
+            st.rerun()
         else:
             st.session_state["password_correct"] = False
             st.error("😕 User not known or password incorrect")
 
-    # Check if the login form should be displayed.
     if st.session_state.get("show_login_form", True):
         login_form()
 
