1a1fe7d92b9012c7082e3c5e40e4eda588694a1d
pterletskiy <petr.terletskiy@gmail.com>
2024-01-02 16:34:42 +0000

    home update

--- webpage/home.py
+++ webpage/home.py
@@ -10,6 +10,17 @@ def app():
     st.write("AutoMentor is more than just a technology company, it is your personal assistant in the world of cars.")
 
     st.write('---')
-    st.write("""Whether you are looking for a new or a used car, a sports car or a family car, a diesel or an electric one,
-            AutoMentor will help you find the best match for your needs and preferences. Chat with our friendly and smart bot, and let AutoMentor do the rest!""")
+    st.write('''Whether you are looking for a new or a used car, a sports car or a family car, a diesel or an electric one,
+            AutoMentor will help you find the best match for your needs and preferences. Chat with our friendly and smart bot, and let AutoMentor do the rest!''')
     
+    st.write('---')
+    st.write('''If you are a car buyer who values convenience and customization, this is the right Chatbot for you.
+             You can simply input your desired features and preferences for a car, and our Chatbot will instantly deliver the most suitable options based on your criteria.
+             Our Chatbot is designed to make car shopping easy and enjoyable for everyone.''')
+    
+    st.write('''AutoMentor is the ultimate solution for busy car buyers who want to find their perfect car in a snap.
+             By scanning various car selling platforms and analyzing the desired features, web reviews, and user feedback,
+             AutoMentor can recommend the best car options for each customer in less than a minute. AutoMentor makes car shopping easy, fast, and fun!''')
+
+    st.write('---')
+    st.write('Create your account to start using AutoMentor! We will need your name, email and location.')
