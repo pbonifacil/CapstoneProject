a1dc660e6f854af075aa42f638d7471db035f089
pterletskiy <petr.terletskiy@gmail.com>
2024-01-02 11:20:38 +0000

    contacts update

--- webpage/contacts.py
+++ webpage/contacts.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 def app():
-    st.caption('Contacts')
+    st.caption('📞 | Contacts')
     st.write('---')
 
-    st.subheader('Contact Us')
     st.write('If you have any questions, please contact us at support@automentor.com.')
 
