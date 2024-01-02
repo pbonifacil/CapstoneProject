d608fec334f3e3d5f25ccff0a2ff7a21349f8f76
pterletskiy <petr.terletskiy@gmail.com>
2024-01-02 11:15:12 +0000

    home update

--- webpage/home.py
+++ webpage/home.py
@@ -1,10 +1,14 @@
 import streamlit as st
 
 def app():
-    st.caption('Home Page')
+
+    st.caption('🏠 | Home Page')
 
     st.title('AutoMentor')
     st.subheader('The ultimate guide to your dream car!')
     st.write("AutoMentor is more than just a technology company, it is your personal assistant in the world of cars.")
+
+    st.write('---')
     st.write("""Whether you are looking for a new or a used car, a sports car or a family car, a diesel or an electric one,
-                AutoMentor will help you find the best match for your needs and preferences. Chat with our friendly and smart bot, and let AutoMentor do the rest!""")
+            AutoMentor will help you find the best match for your needs and preferences. Chat with our friendly and smart bot, and let AutoMentor do the rest!""")
+
