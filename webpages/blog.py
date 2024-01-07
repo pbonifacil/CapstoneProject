97a18481cfde790e5d3c1cfbc90351bca30004fc
pterletskiy <petr.terletskiy@gmail.com>
2024-01-07 20:10:42 +0000

    blog posts update

--- webpages/blog.py
+++ webpages/blog.py
@@ -4,33 +4,33 @@ def app():
     st.caption('📝 | Blog')
     st.write('---')
 
-    page = st.selectbox("Some Blog Posts:", 
-                        ["-", "Blog 1", "Blog 2", "Blog 3", "Blog 4", "Blog 5"])
+    page = st.selectbox("Our Blog Posts:", 
+                        ["-", "The Future of Car Shopping", "Tech Enthusiast's Guide", "Green Driving", "Mastering the Used Car Market", "Empowering First-Time Buyers"])
 
     if page == "-":
         st.write(' ')
     
-    if page == "Blog 1":
+    if page == "The Future of Car Shopping":
         st.subheader("The Future of Car Shopping: A Dive into AutoMentor's Chatbot Technology")
         st.write('Explore how AutoMentor is reshaping the car-buying experience with its cutting-edge Chatbot technology. Discuss the benefits of personalized recommendations and the impact on the future of automotive retail.')
     
 
-    if page == "Blog 2":
+    if page == "Tech Enthusiast's Guide":
         st.subheader("Tech Enthusiast's Guide: Navigating the Automotive Landscape with AutoMentor")
         st.write('Tailored for tech-savvy individuals, this post explores how AutoMentor caters to the preferences of those who appreciate innovation and efficiency in the car-buying process.')
 
 
-    if page == "Blog 3":
+    if page == "Green Driving":
         st.subheader('Green Driving: Exploring Eco-Friendly Options with AutoMentor')
         st.write("Highlight AutoMentor's commitment to sustainability by showcasing its recommendations for eco-friendly car options. Discuss the growing importance of environmentally conscious choices in the automotive industry.")
 
 
-    if page == "Blog 4":
+    if page == "Mastering the Used Car Market":
         st.subheader("Mastering the Used Car Market: AutoMentor's Transparency Advantage")
         st.write('Shed light on how AutoMentor addresses the challenges of the used car market by providing transparent information and reliable recommendations, making the process smoother for buyers.')
 
 
-    if page == "Blog 5":
+    if page == "Empowering First-Time Buyers":
         st.subheader("Empowering First-Time Buyers: AutoMentor's Guide to Confident Car Purchases")
         st.write("Offer insights and tips for first-time car buyers, emphasizing how AutoMentor's user-friendly interface and personalized guidance simplify the decision-making process for those new to the world of car ownership.")
 
